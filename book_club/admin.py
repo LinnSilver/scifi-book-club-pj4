9a895ac3a25d6640c7f6ed6f170ff78fa5b9b618
LinnSilver <baralinn@gmail.com>
2023-05-11 09:23:38 +0000

    Create superuser

--- book_club/admin.py
+++ book_club/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from posts.models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    pass  # This just tells Django to use the default settings.
+
+    admin.site.register(Post, PostAdmin)
