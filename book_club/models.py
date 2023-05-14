71f966815f4447568d70355f1cee51567590414d
LinnSilver <baralinn@gmail.com>
2023-05-14 08:53:41 +0000

    Update book class

--- book_club/models.py
+++ book_club/models.py
@@ -1,30 +1,25 @@
 from django.db import models
+from django.utils.text import slugify
 from django.contrib.auth.models import User
 
 
-class Post(models.Model):
+class book(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    content = models.TextField()
+    author = models.CharField(max_length=100)
+    description = models.TextField()
+    likes = models.ManyToManyField(User, related_name='liked_books', blank=True)
+    average_rating = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
     slug = models.SlugField(max_length=50, unique=True, default='')
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)  # Generate slug from the title if it's not provided
-        super().save(*args, **kwargs)
-
     status = models.CharField(max_length=10, choices=(
         ('draft', 'Draft'),
         ('published', 'Published'),
     ))
     created_on = models.DateTimeField(auto_now_add=True)
 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)  # Generate slug from the title if it's not provided
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return self.title
-
-
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    description = models.TextField()
-    likes = models.ManyToManyField(User, related_name='liked_books', blank=True)
-    average_rating = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
