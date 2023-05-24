e8263e2866a2c04c137ff6ae318e4de971c0ba48
LinnSilver <baralinn@gmail.com>
2023-05-24 08:28:48 +0000

    Add style to url

--- book_club/urls.py
+++ book_club/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from . import views
-from .views import logoutView
-from .views import ManagerView
-from .views import manager
-from .views import LoginView
+from .views import logoutView, ManagerView, manager, LoginView
 from .views import handler404, handler500, handler403, handler405
 
 
@@ -11,9 +8,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('logout/', views.logout_view, name='logout'),
     path('manager/', ManagerView.as_view(), name='manager'),
-    path('manager/', manager, name='manager'),
     path('login/', LoginView.as_view(), name='login'),
-
 ]
 
 handler404 = 'book_klub.views.handler404'
